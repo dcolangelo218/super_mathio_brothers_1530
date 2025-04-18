90345c7bb48acce9743ade74af9a37069fa4e00d
LucynaKushinada <125412728+LucynaKushinada@users.noreply.github.com>
2025-04-18 18:12:44 -0400

    Create Tutorial.java

---
+++ Archived Files/java-backend/src/main/java/Tutorial.java
@@ -0,0 +1,5 @@
+public class Tutorial extends Level {
+    private String topic;
+    public Tutorial(String topic) { super(0); this.topic = topic; }
+    public String getTopic() { return topic; }
+}
